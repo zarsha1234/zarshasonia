d02414659642afda13553f974b50c464bf76c566
zarsha123 <zarshasonia560@gmail.com>
2024-09-27 11:30:16 +0500

    my first

---
+++ lib/todo_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule TodoWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use TodoWeb, :controller` and
+  `use TodoWeb, :live_view`.
+  """
+  use TodoWeb, :html
+
+  embed_templates "layouts/*"
+end
